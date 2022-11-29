f283f1ba7f46949524b72c2245657c35c49b2dd9
totenk0pf <nguyenhuuquang2608@gmail.com>
2022-11-29 11:09:47 +0700

    ultrakill

--- Assets/Scripts/Game/Animation/BikeRig.cs
+++ Assets/Scripts/Game/Animation/BikeRig.cs
@@ -1,33 +1,61 @@
-﻿using System.Collections;
+﻿using System;
 using Core.Logging;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Game.Animation {
+    [Flags]
+    public enum RigState {
+        Moving,
+        Wheelie,
+        Stopped
+    }
+    
     public class BikeRig : MonoBehaviour {
-        [TitleGroup("Transforms")]
+        [ShowInInspector] [ReadOnly] private RigState state;
+        
+        [TitleGroup("Transforms")] 
         [SerializeField] private Transform root;
         [SerializeField] private Transform headAxis;
         [SerializeField] private Transform swerveAxis;
         [SerializeField] private Transform[] wheels;
 
-        [TabGroup("Settings", "Head")] [SerializeField] private float headRotateSpeed;
-        [TabGroup("Settings", "Head")] [SerializeField] private float maxHeadAngle;
+        [TabGroup("Settings", "Head")] [SerializeField]
+        private float headRotateSpeed;
 
-        [TabGroup("Settings", "Body")] [TitleGroup("Settings/Body/Speed")] [SerializeField] private float bodyLiftSpeed;
-        [TabGroup("Settings", "Body")] [TitleGroup("Settings/Body/Speed")] [SerializeField] private float bodyRotateSpeed;
-        [TabGroup("Settings", "Body")] [TitleGroup("Settings/Body/Speed")] [SerializeField] private float bodyLeanSpeed;
-        
-        [TabGroup("Settings", "Body")] [TitleGroup("Settings/Body/Angle")] [SerializeField] private float maxBodyAngle;
-        [TabGroup("Settings", "Body")] [TitleGroup("Settings/Body/Angle")] [SerializeField] private float maxBodyPitch;
-        [TabGroup("Settings", "Body")] [TitleGroup("Settings/Body/Angle")] [SerializeField] private float maxLeanAngle;
-        
-        [TabGroup("Settings", "Body")] [TitleGroup("Settings/Body/Delay")] [SerializeField] private float rotateDelay;
-        [TabGroup("Settings", "Body")] [TitleGroup("Settings/Body/Delay")] [SerializeField] private float leanDelay;
+        [TabGroup("Settings", "Head")] [SerializeField]
+        private float maxHeadAngle;
 
-        [TitleGroup("Equilibrium settings")] 
-        [SerializeField] private float returnSpeed;
-        
+        [TabGroup("Settings", "Body")] [TitleGroup("Settings/Body/Speed")] [SerializeField]
+        private float bodyLiftSpeed;
+
+        [TabGroup("Settings", "Body")] [TitleGroup("Settings/Body/Speed")] [SerializeField]
+        private float bodyRotateSpeed;
+
+        [TabGroup("Settings", "Body")] [TitleGroup("Settings/Body/Speed")] [SerializeField]
+        private float bodyLeanSpeed;
+
+        [TabGroup("Settings", "Body")] [TitleGroup("Settings/Body/Angle")] [SerializeField]
+        private float maxBodyAngle;
+
+        [TabGroup("Settings", "Body")] [TitleGroup("Settings/Body/Angle")] [SerializeField]
+        private float maxBodyPitch;
+
+        [TabGroup("Settings", "Body")] [TitleGroup("Settings/Body/Angle")] [SerializeField]
+        private float maxLeanAngle;
+
+        [TabGroup("Settings", "Body")] [TitleGroup("Settings/Body/Delay")] [SerializeField]
+        private float rotateDelay;
+
+        [TabGroup("Settings", "Body")] [TitleGroup("Settings/Body/Delay")] [SerializeField]
+        private float leanDelay;
+
+        [TabGroup("Settings", "Wheels")] [SerializeField]
+        private float wheelSpeed;
+
+        [TitleGroup("Equilibrium settings")] [SerializeField]
+        private float returnSpeed;
+
         private float _baseHeadAngle;
         private float _headAngle;
         private Vector3 _clampVector;
@@ -42,9 +70,10 @@ namespace Game.Animation {
 
         private float _currentRotateDelay;
         private float _currentLeanDelay;
-        
+
 
         private void Awake() {
+            state             = RigState.Moving;
             _baseHeadAngle    = headAxis.localEulerAngles.x;
             _baseHeadRotation = headAxis.localRotation;
             _baseBodyRotation = swerveAxis.localRotation;
@@ -55,21 +84,18 @@ namespace Game.Animation {
 
         private void Update() {
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) != 0f) {
-                if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.9f) {
+                if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.9f) {
                     _currentRotateDelay += Time.deltaTime;
-                    _currentLeanDelay += Time.deltaTime;
-                } else {
-                    _currentRotateDelay = 0;
-                    _currentLeanDelay   = 0;
+                    _currentLeanDelay   += Time.deltaTime;
                 }
                 RotateHead();
                 RotateBody();
-                LiftBody();
             } else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) != 0f) {
-                
+                LiftBody();
             } else {
                 ReturnToEquilibrium();
             }
+            RotateWheels();
             NCLogger.Log($"{Input.GetAxis("Horizontal")}");
         }
 
@@ -80,8 +106,8 @@ namespace Game.Animation {
             var euler = headAxis.localEulerAngles;
             _headAngle = euler.x;
             _headAngle = Mathf.Clamp(_headAngle,
-                                         _baseHeadAngle + -maxHeadAngle,
-                                         _baseHeadAngle + maxHeadAngle);
+                                     _baseHeadAngle + -maxHeadAngle,
+                                     _baseHeadAngle + maxHeadAngle);
             if (Mathf.Abs(_baseHeadAngle + maxHeadAngle - _headAngle) < 0.1f) {
                 if (_clampVector == Vector3.zero) _clampVector = euler;
             } else {
@@ -104,29 +130,51 @@ namespace Game.Animation {
                 _bodyLean -= Input.GetAxisRaw("Horizontal") * bodyLeanSpeed * Time.deltaTime;
                 _bodyLean =  Mathf.Clamp(_bodyLean, -maxLeanAngle, maxLeanAngle);
             }
-            
+
             swerveAxis.localEulerAngles = new Vector3(0f, _bodyAngle, _bodyLean);
         }
 
-        private void ReturnToEquilibrium() {
-            swerveAxis.localRotation = Quaternion.Lerp(swerveAxis.localRotation, _baseBodyRotation, returnSpeed * Time.deltaTime);
-            headAxis.localRotation   = Quaternion.Lerp(headAxis.localRotation, _baseHeadRotation, returnSpeed * Time.deltaTime);
-            root.localRotation      = Quaternion.Lerp(root.localRotation, _baseRootRotation, returnSpeed * Time.deltaTime);
+        private void ReturnToEquilibrium(bool yawOnly = false) {
+            swerveAxis.localRotation = Quaternion.Lerp(swerveAxis.localRotation, 
+                                                       _baseBodyRotation, 
+                                                       returnSpeed * Time.deltaTime);
+            _bodyAngle = Mathf.SmoothStep(_bodyAngle, 0f, returnSpeed * Time.deltaTime);
+            _bodyLean  = Mathf.SmoothStep(_bodyLean, 0f, returnSpeed * Time.deltaTime);
+
+            headAxis.localRotation = Quaternion.Lerp(headAxis.localRotation, 
+                                                     _baseHeadRotation, 
+                                                     returnSpeed * Time.deltaTime);
+            _headAngle = Mathf.Lerp(_headAngle, 0f, returnSpeed * Time.deltaTime);
             
+            if (yawOnly) {
+                root.localRotation = Quaternion.Lerp(root.localRotation, 
+                                                     _baseRootRotation, 
+                                                     returnSpeed * Time.deltaTime);
+                _bodyPitch         = Mathf.Lerp(_bodyPitch, 0f, returnSpeed * Time.deltaTime);
+            }
+
             _clampVector = Vector3.zero;
-            _headAngle   = Mathf.Lerp(_headAngle, 0f, returnSpeed * Time.deltaTime);
-            _bodyAngle   = Mathf.Lerp(_bodyAngle, 0f, returnSpeed * Time.deltaTime);
-            _bodyLean    = Mathf.Lerp(_bodyLean, 0f, returnSpeed * Time.deltaTime);
-            _bodyPitch   = Mathf.Lerp(_bodyPitch, 0f, returnSpeed * Time.deltaTime);
         }
 
         private void LiftBody() {
             // if (Mathf.Abs(_headAngle - _baseHeadAngle) > 2f ||
-                // Mathf.Abs(_bodyAngle - _baseBodyAngle) > 2f) return;
+            // Mathf.Abs(_bodyAngle - _baseBodyAngle) > 2f) return;
+            if (_bodyPitch > 1f) {
+                state |= RigState.Wheelie;
+            } else {
+                state &= ~RigState.Wheelie;
+            }
             var euler = root.localEulerAngles;
             _bodyPitch            -= Input.GetAxis("Vertical") * bodyLiftSpeed * Time.deltaTime;
-            _bodyPitch            =  Mathf.Clamp(_bodyPitch, -maxBodyAngle, 0);
+            _bodyPitch            =  Mathf.Clamp(_bodyPitch, -maxBodyPitch, 0);
             root.localEulerAngles =  new Vector3(_bodyPitch, euler.y, euler.z);
+        }
+
+        private void RotateWheels() {
+            if (wheels.Length < 1) return;
+            foreach (var i in wheels) {
+                i.localEulerAngles += new Vector3(wheelSpeed * Time.deltaTime, 0f, 0f);
+            }
         }
     }
 }
