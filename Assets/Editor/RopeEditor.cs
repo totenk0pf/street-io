cacc530904bd8aeae4fdc76516ff681cfa4eabed
Huu Quang Nguyen <nguyenhuuquang2608@gmail.com>
2023-05-21 00:00:32 +0700

    rope

---
+++ Assets/Editor/RopeEditor.cs
@@ -0,0 +1,47 @@
+﻿using Game;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(RopeBehaviour))]
+public class RopeEditor : Editor {
+    protected virtual void OnSceneGUI() {
+        switch (Event.current.type) {
+            case EventType.Repaint:
+                RopeBehaviour rope = (RopeBehaviour) target;
+                DrawArrows(rope.start, 0, HandleUtility.GetHandleSize(rope.start));
+                DrawArrows(rope.end, 1, HandleUtility.GetHandleSize(rope.end));
+                break;
+            
+            case EventType.Layout:
+                
+                break;
+        }
+    }
+
+    private void DrawArrows(Vector3 pos, int id, float size = 1f) {
+        Handles.color = Handles.xAxisColor;
+        Handles.ArrowHandleCap(
+            id,
+            pos,
+            Quaternion.identity * Quaternion.LookRotation(Vector3.right),
+            size,
+            EventType.Repaint
+        );
+        Handles.color = Handles.yAxisColor;
+        Handles.ArrowHandleCap(
+            id,
+            pos,
+            Quaternion.identity * Quaternion.LookRotation(Vector3.up),
+            size,
+            EventType.Repaint
+        );
+        Handles.color = Handles.zAxisColor;
+        Handles.ArrowHandleCap(
+            id,
+            pos,
+            Quaternion.identity * Quaternion.LookRotation(Vector3.forward),
+            size,
+            EventType.Repaint
+        );
+    }
+}
---
+++ Assets/Scripts/Game/Rope/RopeBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Game {
+    [Serializable]
+    public struct RopeSegment {
+        public Vector3 prevPos;
+        public Vector3 currPos;
+
+        public RopeSegment(Vector3 pos) {
+            prevPos = pos;
+            currPos = pos;
+        }
+    }
+    
+    [RequireComponent(typeof(LineRenderer))]
+    public class RopeBehaviour : MonoBehaviour {
+        [ReadOnly] public Vector3 start;
+        [ReadOnly] public Vector3 end;
+        
+        [SerializeField] private float segmentLength;
+        
+        private LineRenderer _renderer;
+        private List<RopeSegment> _segments = new ();
+        
+        private const float _debugSphereRadius = 0.2f;
+
+        private void Awake() {
+            _renderer = GetComponent<LineRenderer>();
+        }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmosSelected() {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(start, _debugSphereRadius);
+            Gizmos.DrawWireSphere(end, _debugSphereRadius);
+        }
+#endif
+    }
+}
