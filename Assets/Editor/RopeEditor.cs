5d6e540f14d8189644ecd8f01adee8c7921863c6
Huu Quang Nguyen <50345722+totenk0pf@users.noreply.github.com>
2023-05-22 06:43:25 +0700

    this should fix it

--- Assets/Editor/RopeEditor.cs
+++ Assets/Editor/RopeEditor.cs
@@ -1,47 +1,155 @@
-﻿using Game;
+﻿using System.Collections.Generic;
+using Core.Logging;
+using Game;
+using Sirenix.OdinInspector.Editor;
 using UnityEditor;
 using UnityEngine;
 
 [CustomEditor(typeof(RopeBehaviour))]
-public class RopeEditor : Editor {
+public class RopeEditor : OdinEditor {
+    private enum HandleAxis {
+        X,
+        Y,
+        Z
+    }
+    
+    private struct HandleData {
+        public HandleAxis Axis { get; }
+        public bool Start { get; }
+        public int ID { get; }
+
+        public HandleData(HandleAxis axis, bool start, int id = -1) {
+            Axis  = axis;
+            Start = start;
+            ID    = id == -1 ? GUIUtility.GetControlID(FocusType.Passive) : id;
+        }
+    }
+
+    private class RopeHandle {
+        public HandleData[] Data { get; }
+        public bool Start { get; }
+        public Vector3 Position { get; }
+        public int[] AxisIDs { get; }
+
+        public RopeHandle(Vector3 position, bool start) {
+            Position = position;
+            Start     = start;
+            Data = new[] {
+                new HandleData(HandleAxis.X, start),
+                new HandleData(HandleAxis.Y, start),
+                new HandleData(HandleAxis.Z, start),
+            };
+            AxisIDs = new[] {
+                Data[0].ID,
+                Data[1].ID,
+                Data[2].ID
+            };
+        }
+    }
+    
+    private RopeHandle startHandle;
+    private RopeHandle endHandle;
+    
+    private bool cached;
+    private RopeBehaviour rope;
+    private int nearestID;
+    
     protected virtual void OnSceneGUI() {
+        if (!cached) {
+            rope        = (RopeBehaviour) target;
+            startHandle = new RopeHandle(rope.start, true);
+            endHandle   = new RopeHandle(rope.end, false);
+            cached      = true;
+        }
+        
         switch (Event.current.type) {
             case EventType.Repaint:
-                RopeBehaviour rope = (RopeBehaviour) target;
-                DrawArrows(rope.start, 0, HandleUtility.GetHandleSize(rope.start));
-                DrawArrows(rope.end, 1, HandleUtility.GetHandleSize(rope.end));
+                DrawPointGizmo(rope.GetStartPos(), startHandle, EventType.Repaint);
+                DrawPointGizmo(rope.GetEndPos(), endHandle, EventType.Repaint);
                 break;
             
             case EventType.Layout:
-                
+                DrawPointGizmo(rope.GetStartPos(), startHandle, EventType.Layout);
+                DrawPointGizmo(rope.GetEndPos(), endHandle, EventType.Layout);
+                break;
+            
+            case EventType.MouseDown:
+                nearestID = HandleUtility.nearestControl;
+                break;
+            
+            case EventType.MouseUp:
+                nearestID = -1;
+                break;
+            
+            case EventType.MouseDrag:
+                if (Event.current.button == 0) {
+                    
+                }
                 break;
         }
     }
 
-    private void DrawArrows(Vector3 pos, int id, float size = 1f) {
-        Handles.color = Handles.xAxisColor;
+    private void DrawPointGizmo(Vector3 point, RopeHandle handle, EventType eventType) {
+        DrawArrows(point, handle.Data, eventType, 0.5f);
+        var label = handle.Start ? "Start" : "End";
+        DrawLabels(point, label);
+    }
+
+    private void DrawArrows(Vector3 pos, HandleData[] data, EventType eventType, float size = 1f) {
+        // Vector4 drawPos = rope.transform.worldToLocalMatrix * pos;
+        HandleData xData = data[0];
+        HandleData yData = data[1];
+        HandleData zData = data[2];
+        if (IsEventRepaint()) Handles.color = GetHandleColor(xData);
         Handles.ArrowHandleCap(
-            id,
+            xData.ID,
             pos,
             Quaternion.identity * Quaternion.LookRotation(Vector3.right),
             size,
-            EventType.Repaint
+            eventType
         );
-        Handles.color = Handles.yAxisColor;
+        if (IsEventRepaint()) Handles.color = GetHandleColor(yData);
         Handles.ArrowHandleCap(
-            id,
+            yData.ID,
             pos,
             Quaternion.identity * Quaternion.LookRotation(Vector3.up),
             size,
-            EventType.Repaint
+            eventType
         );
-        Handles.color = Handles.zAxisColor;
+        if (IsEventRepaint()) Handles.color = GetHandleColor(zData);
         Handles.ArrowHandleCap(
-            id,
+            zData.ID,
             pos,
             Quaternion.identity * Quaternion.LookRotation(Vector3.forward),
             size,
-            EventType.Repaint
+            eventType
         );
+    }
+    
+    private void DrawLabels(Vector3 pos, string text, float distance = 1f) {
+        Handles.Label(pos + new Vector3(0f, distance, 0f), text);
+    }
+
+    private static bool IsEventRepaint() => Event.current.type == EventType.Repaint;
+    
+    private Color GetHandleColor(HandleData data) {
+        Color returnColor = default;
+        if (nearestID == data.ID) return Color.yellow;
+        switch (data.Axis) {
+            case HandleAxis.X:
+                returnColor = Handles.xAxisColor;
+                break;
+            case HandleAxis.Y:
+                returnColor = Handles.yAxisColor;
+                break;
+            case HandleAxis.Z:
+                returnColor = Handles.zAxisColor;
+                break;
+        }
+        return returnColor;
+    }
+
+    private RopeHandle GetNearestHandle() {
+        return null;
     }
 }
--- Assets/Scripts/Game/Rope/RopeBehaviour.cs
+++ Assets/Scripts/Game/Rope/RopeBehaviour.cs
@@ -1,41 +1,149 @@
 ﻿using System;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
+using UnityEditor;
 using UnityEngine;
 
 namespace Game {
     [Serializable]
-    public struct RopeSegment {
+    public struct RopeVertex {
         public Vector3 prevPos;
         public Vector3 currPos;
 
-        public RopeSegment(Vector3 pos) {
+        public RopeVertex(Vector3 pos) {
             prevPos = pos;
             currPos = pos;
         }
     }
     
+    [ExecuteAlways]
     [RequireComponent(typeof(LineRenderer))]
-    public class RopeBehaviour : MonoBehaviour {
-        [ReadOnly] public Vector3 start;
-        [ReadOnly] public Vector3 end;
+    public class RopeBehaviour : SerializedMonoBehaviour {
+        [TitleGroup("Rope ends")]
+        [ReadOnly] public Vector3 start = new (1f, 0f, 0f);
+        [ReadOnly] public Vector3 end = new (-1f, 0f, 0f);
+
+        [TitleGroup("Forces")]
+        [SerializeField] private float gravity = 9.81f;
+        [SerializeField] private float damping = 0.5f;
+        private Vector3 _grav;
         
-        [SerializeField] private float segmentLength;
+        [TitleGroup("Simulation settings")]
+        [SerializeField] private int segmentCount = 4;
+        [SerializeField] private int iterations = 16;
+        [SerializeField] private float sagAmount = 0f; 
+        [ReadOnly] [ShowInInspector] private float segmentLength;
         
         private LineRenderer _renderer;
-        private List<RopeSegment> _segments = new ();
+        private List<RopeVertex> segments = new ();
         
-        private const float _debugSphereRadius = 0.2f;
+        private const float _gizmoSize = 0.2f;
 
         private void Awake() {
             _renderer = GetComponent<LineRenderer>();
+            var dist = Vector3.Distance(start, end);
+            segmentLength = dist / segmentCount;
+            for (var i = 0; i < segmentCount; i++) {
+                segments.Add(new RopeVertex(GetStartPos() + new Vector3(i * segmentLength + sagAmount, 0f, 0f)));
+            }
+            _grav = new Vector3(0f, gravity, 0f);
         }
 
+        private void Update() {
+            if (!Application.isPlaying) return;
+            Render();
+        }
+
+        private void FixedUpdate() {
+            if (!Application.isPlaying) return;
+            Simulate();
+        }
+
+        private void Render() {
+            var ropeVertices = new Vector3[segmentCount];
+            for (var i = 0; i < segmentCount; i++) {
+                ropeVertices[i] = segments[i].currPos;
+            }
+            _renderer.positionCount = ropeVertices.Length;
+            _renderer.SetPositions(ropeVertices);
+        }
+
+        private void Simulate() {
+            for (var i = 0; i < segmentCount; i++) {
+                RopeVertex vert = segments[i];
+                Vector3 delta = vert.currPos - vert.prevPos;
+                vert.prevPos =  vert.currPos;
+                vert.currPos += _grav * Time.deltaTime + delta;
+                segments[i]  =  vert;
+            }
+            for (var i = 0; i < iterations; i++) {
+                Constrain();
+            }
+        }
+
+        private void Constrain() {
+            RopeVertex first = segments[0];
+            first.currPos = GetStartPos();
+            segments[0]  = first;
+            
+            RopeVertex last = segments[^1];
+            last.currPos  = GetEndPos();
+            segments[^1] = last;
+            
+            for (var i = 0; i < segmentCount - 1; i++) {
+                RopeVertex currVert = segments[i];
+                RopeVertex nextVert = segments[i + 1];
+            
+                var dist = (currVert.currPos - nextVert.currPos).magnitude;
+                var error = dist - segmentLength + sagAmount;
+                Vector3 changeDir = (currVert.currPos - nextVert.currPos).normalized;
+                Vector3 changeAmt = changeDir * error;
+                Vector3 half = changeAmt * damping;
+                if (i != 0) {
+                    currVert.currPos  -= half;
+                    segments[i]     =  currVert;
+                    nextVert.currPos  += half;
+                    segments[i + 1] =  nextVert;
+                } else {
+                    nextVert.currPos  += changeAmt;
+                    segments[i + 1] =  nextVert;
+                }
+            }
+        }
+
+        public Vector3 GetStartPos() => transform.position + start;
+        public Vector3 GetEndPos() => transform.position + end;
+
 #if UNITY_EDITOR
+        private void OnValidate() {
+            if (Application.isPlaying) {
+                
+            } else {
+                LineRenderer tempRenderer = GetComponent<LineRenderer>();
+                if (tempRenderer.positionCount > 2) tempRenderer.positionCount = 0;
+                var gizmoVertices = new Vector3[2];
+                gizmoVertices[0] = GetStartPos();
+                gizmoVertices[1] = GetEndPos();
+                tempRenderer.SetPositions(gizmoVertices);
+            }
+        }
+
         private void OnDrawGizmosSelected() {
             Gizmos.color = Color.yellow;
-            Gizmos.DrawWireSphere(start, _debugSphereRadius);
-            Gizmos.DrawWireSphere(end, _debugSphereRadius);
+            Vector3 drawSize = new (_gizmoSize, _gizmoSize, _gizmoSize);
+            Gizmos.DrawWireCube(GetStartPos(), drawSize);
+            Gizmos.DrawWireCube(GetEndPos(), drawSize);
+            var dir = (GetEndPos() - GetStartPos()).normalized;
+            // Gizmos.color = Color.green;
+            if (Application.isPlaying) {
+                for (var i = 1; i < segmentCount - 1; i++) {
+                    Gizmos.DrawWireSphere(segments[i].currPos, _gizmoSize * 0.3f);
+                }
+            } else {
+                for (var i = 1; i < segmentCount; i++) {
+                    Gizmos.DrawWireSphere(GetStartPos() + dir * (Vector3.Distance(start, end) / segmentCount) * i, _gizmoSize * 0.3f);
+                }
+            }
         }
 #endif
     }
